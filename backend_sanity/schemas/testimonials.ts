20a57890718001641a5f2097c0dbc33ed6c52729
alejo-RB <rodrgueza@yahoo.es>
2023-09-23 00:18:33 -0500

    Update Testimonial.jsx
    Se añadieron las imagenes de las personas que hicieron las recomendaciones

--- backend_sanity/schemas/testimonials.ts
+++ backend_sanity/schemas/testimonials.ts
@@ -14,7 +14,7 @@ export default {
       type: 'string',
     },
     {
-      name: 'imageurl',
+      name: 'imgurl',
       title: 'ImgURL',
       type: 'image',
       options: {
--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -11,6 +11,10 @@ const Testimonial = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [currenIndex, setCurrenIndex] = useState(0)
 
+const handleClick = (index) => {
+  setCurrenIndex(index)
+}
+
   useEffect(() => {
     const query = '*[_type == "testimonials"]';
     const brandsQuery = '*[_type == "brands"]';
@@ -24,13 +28,45 @@ const Testimonial = () => {
     });
   }, []);
 
+  const test = testimonials[currenIndex];
+
   return (
     <>
       {testimonials.length && (
         <>
-          <div className="app__testimonial-item app__flex"></div>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(test.imgurl)} alt="testimonials" />
+            <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+              <div>
+                <h4 className="bold-text">{test.name}</h4>
+                <h5 className="p-text">{test.company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currenIndex == 0 ? testimonials.length - 1 : currenIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+            <div className="app__flex" onClick={() => handleClick(currenIndex == testimonials.length -1 ? 0 : currenIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
         </>
       )}
+
+      <div className="app__testimonial-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={brand._id}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
     </>
   );
 };
