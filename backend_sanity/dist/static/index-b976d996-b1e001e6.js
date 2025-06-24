b3392db6ec943328466e26b8cd7947f578fc3c42
alejo-RB <rodrgueza@yahoo.es>
2025-06-24 18:34:29 -0500

    update

--- backend_sanity/dist/static/index-b976d996-b1e001e6.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-0de920bc-46a47bed.js";import{D as m}from"./sanity-61659224.js";import"./json-inspector-bfac7cbd.js";export{m as DocumentPaneProvider,t as default};
